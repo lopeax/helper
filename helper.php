bacd87a13bd936c030e2dcb8f0fc5dc8423f5364
Tony Lopez <lopeax@users.noreply.github.com>
2016-06-06 15:10:53 +0100

    Update helper.php

--- helper.php
+++ helper.php
@@ -10,6 +10,16 @@ function excerptify($text, $length = 140){
     }
 }
 
+// Search for any urls in the text and replace them with a linked version
+function linkify($text){
+    $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+    if(preg_match($reg_exUrl, $text, $url)) {
+       return preg_replace($reg_exUrl, "<a href=".$url[0].">{$url[0]}</a> ", $text);
+    } else {
+       return $text;
+    }
+}
+
 // Gets today's item from an array
 function todays_item($arr){
     $count = count($arr);
